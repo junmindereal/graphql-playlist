95ac03efa4444af0d4de8cdacedf80b5ca129c6e
Junmin De Real <junmindereal@gmail.com>
2020-07-21 11:48:45 +0800

    Connecting to mongoDB Atlas

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:12345@training.viq6y.mongodb.net/Training?retryWrites=true&w=majority",
+    options
+  )
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.log("Error: ", err.message));
 
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
