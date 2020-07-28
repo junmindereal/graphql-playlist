262210d3bd69b65ef02126597a320530473d656f
Junmin De Real <junmindereal@gmail.com>
2020-07-28 14:28:20 +0800

    Apollo client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import BookList from "./components/BookList";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
