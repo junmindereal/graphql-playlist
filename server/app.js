c2d5ec56e687e13b42d054b6d99e1d0f134ff756
Junmin De Real <junmindereal@gmail.com>
2020-09-11 00:22:12 +0800

    Added dotenv and removed secrets in code

--- server/app.js
+++ server/app.js
@@ -1,25 +1,26 @@
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
+const {graphqlHTTP} = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require('dotenv').config();
 
 const app = express();
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
+const options = {useNewUrlParser: true, useUnifiedTopology: true};
 
 // allow cross-origin requests
 app.use(cors());
 
 mongoose
-  .connect(
-    "mongodb+srv://admin:12345@training.viq6y.mongodb.net/Training?retryWrites=true&w=majority",
-    options
-  )
-  .then(() => console.log("Connected to MongoDB Atlas"))
-  .catch((err) => console.log("Error: ", err.message));
+    .connect(
+        process.env.MONGO_URI,
+        options
+    )
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch((err) => console.log("Error: ", err.message));
 
-app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+app.use("/graphql", graphqlHTTP({schema, graphiql: true}));
 
 app.listen(4000, () => {
-  console.log("now listening for requests on port 4000");
+    console.log("now listening for requests on port 4000");
 });
